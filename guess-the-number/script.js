741dc09109155ab1c88d10a7faf5a7355b30821e
Bikash Moktan <moktan.bi@gmail.com>
2025-09-19 19:48:31 +0400

    game over  update and IIFI

--- guess-the-number/script.js
+++ guess-the-number/script.js
@@ -1,39 +1,47 @@
-const submit = document.querySelector(".submit");
-const startGame = document.querySelector(".startGame");
-const userInput = document.querySelector(".user-input");
-const form = document.querySelector("form");
-const result = document.querySelector(".result");
-const allGuessess = document.querySelector(".allGuesses span");
+(() => {
+  const submit = document.querySelector(".submit");
+  const startGame = document.querySelector(".startGame");
+  const userInput = document.querySelector(".user-input");
+  const form = document.querySelector("form");
+  const result = document.querySelector(".result");
+  const allGuessess = document.querySelector(".allGuesses span");
 
-let allGuess = [];
-let randomNum = parseInt(Math.floor(Math.random() * 100));
+  let allGuess = [];
+  let randomNum = parseInt(Math.floor(Math.random() * 100));
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const inputNumber = parseInt(userInput.value);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputNumber = parseInt(userInput.value);
 
-  if (randomNum < inputNumber) {
-    result.innerText = "Too High! 📈";
-  } else if (randomNum > inputNumber) {
-    result.innerText = "Too low! 📉";
-  } else {
-    result.innerText = "You got it! Congrats 🥳";
-    startGame.disabled = false;
-    submit.disabled = true;
-  }
+    if (randomNum < inputNumber) {
+      result.innerText = "Too High! 📈";
+    } else if (randomNum > inputNumber) {
+      result.innerText = "Too low! 📉";
+    } else {
+      result.innerText = "You got it! Congrats 🥳";
+      startGame.disabled = false;
+      submit.disabled = true;
+    }
 
-  allGuess.push(inputNumber);
+    allGuess.push(inputNumber);
 
-  allGuessess.innerText = allGuess;
-});
+    allGuessess.innerText = allGuess;
 
-startGame.addEventListener("click", () => {
-  result.innerText = "";
-  allGuessess.innerText = "";
-  userInput.value = "";
-  startGame.disabled = true;
-  submit.disabled = false;
-  //   allGuess.length = 0;
-  allGuess = [];
-  randomNum = parseInt(Math.floor(Math.random() * 100));
-});
+    if (allGuess.length === 8) {
+      result.innerText = "Game Over! 🤣";
+      startGame.disabled = false;
+      submit.disabled = true;
+    }
+  });
+
+  startGame.addEventListener("click", () => {
+    result.innerText = "";
+    allGuessess.innerText = "";
+    userInput.value = "";
+    startGame.disabled = true;
+    submit.disabled = false;
+    //   allGuess.length = 0;
+    allGuess = [];
+    randomNum = parseInt(Math.floor(Math.random() * 100));
+  });
+})();
