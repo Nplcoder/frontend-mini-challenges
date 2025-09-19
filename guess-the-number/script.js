352d24baa3f494f4bad475e01247d255bad38537
Bikash Moktan <moktan.bi@gmail.com>
2025-09-19 18:34:06 +0400

    update

---
+++ guess-the-number/script.js
@@ -0,0 +1,39 @@
+const submit = document.querySelector(".submit");
+const startGame = document.querySelector(".startGame");
+const userInput = document.querySelector(".user-input");
+const form = document.querySelector("form");
+const result = document.querySelector(".result");
+const allGuessess = document.querySelector(".allGuesses span");
+
+let allGuess = [];
+let randomNum = parseInt(Math.floor(Math.random() * 100));
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputNumber = parseInt(userInput.value);
+
+  if (randomNum < inputNumber) {
+    result.innerText = "Too High! 📈";
+  } else if (randomNum > inputNumber) {
+    result.innerText = "Too low! 📉";
+  } else {
+    result.innerText = "You got it! Congrats 🥳";
+    startGame.disabled = false;
+    submit.disabled = true;
+  }
+
+  allGuess.push(inputNumber);
+
+  allGuessess.innerText = allGuess;
+});
+
+startGame.addEventListener("click", () => {
+  result.innerText = "";
+  allGuessess.innerText = "";
+  userInput.value = "";
+  startGame.disabled = true;
+  submit.disabled = false;
+  //   allGuess.length = 0;
+  allGuess = [];
+  randomNum = parseInt(Math.floor(Math.random() * 100));
+});
